53035d26fe483513c894e9b40597bfa5d8fa1b98
Abdullah <69305156+abdoollaahh@users.noreply.github.com>
2024-09-01 02:54:17 +0500

    feat: add images

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,19 +1,12 @@
-import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from "next/image"
+import HeroImage from "/public/images/sellCar.jpg"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            SPACECAR
-          </Heading>
-        </span>
-      </div>
+      <Image src={HeroImage} alt="Hero Image" layout="fill" objectFit="cover" />
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6"></div>
     </div>
   )
 }
